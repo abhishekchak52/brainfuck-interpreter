c6dc8b40e364b5c405238a54afbdb80dcbf52232
Abhishek Chakraborty <deadbeatfour@gmail.com>
2016-01-06 23:39:31 +0530

    Added help for correct usage
    C-String library included

--- raw/brainfuck-interpreter.cpp
+++ raw/brainfuck-interpreter.cpp
@@ -4,6 +4,10 @@ using namespace std;
 char prog[30000];
 char *ptr = prog;
 int main(int argc, const char* argv[]) {
+	// Checking for correct usage
+	if( argc != 2 || !strcmp(argv[1],"--help") || !strcmp(argv[1],"-h")) // Incorrect usage or help flag
+		cout<<" Usage : ./brainfuck-interpreter [FILE]"<<endl
+				<<" \t [FILE] is a file containing the brainfuck program that is to be interpreted"<<endl;
 	// File handling
 	ifstream program(argv[1],ios::in);
 	while(program)
